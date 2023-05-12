e76b2f4fd11386fb02afae50a87465b62b35fef1
OPISTO\gabriele.gargani <gabri.gargani@gmail.com>
2023-05-12 11:08:16 +0300

    furniture

---
+++ Assets/lighton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lighton : MonoBehaviour
+{
+    interactable1 interact;
+    void Start()
+    {
+        interact = FindObjectOfType<interactable1>();
+        gameObject.SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interact.activated == true) {
+            gameObject.SetActive(true); 
+        }
+    }
+}
